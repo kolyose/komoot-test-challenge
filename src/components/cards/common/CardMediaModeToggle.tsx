735b27eb392f7156654194107310d6e743fe37f2
Sergii Kolisnyk <kolyose@gmail.com>
2025-03-06 10:49:38 +0100

    Change toggle animation

--- src/components/cards/common/CardMediaModeToggle.tsx
+++ src/components/cards/common/CardMediaModeToggle.tsx
@@ -3,18 +3,18 @@ interface CardMediaModeToggleProps {
   onToggle: () => void
 }
 
-export const MEDIA_MODE_TOGGLE_SIZE = 64
+export const MEDIA_MODE_TOGGLE_SIZE = 80
 
 function CardMediaModeToggle({ src, onToggle }: CardMediaModeToggleProps) {
   return (
     <button
       aria-label="Toggle adventure's media view"
       style={{ height: MEDIA_MODE_TOGGLE_SIZE, width: MEDIA_MODE_TOGGLE_SIZE }}
-      className={`absolute right-8 bottom-8 z-10 cursor-pointer overflow-hidden rounded-md bg-white p-1 shadow-md transition-transform duration-300 ease-in-out hover:scale-105`}
+      className={`absolute right-8 bottom-8 z-10 cursor-pointer overflow-hidden rounded-md bg-white p-[5px] shadow-md transition-[padding] duration-300 ease-in-out hover:p-[3px]`}
       onClick={onToggle}
     >
       <img
-        className="max-h-full max-w-full rounded-md object-contain"
+        className="max-h-full max-w-full rounded-md object-cover"
         src={src}
         alt="Media mode toggle thumbnail"
       />
